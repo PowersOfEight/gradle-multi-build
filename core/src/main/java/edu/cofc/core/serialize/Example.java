2382c3e1637377ba910b230ee1e774feb85d856f
Dan Johnson <jdjohnson8883@gmail.com>
2023-04-05 11:00:32 -0400

    Current Xstream Progress

--- core/src/main/java/edu/cofc/core/serialize/Example.java
+++ core/src/main/java/edu/cofc/core/serialize/Example.java
@@ -1,7 +1,17 @@
 package edu.cofc.core.serialize;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Objects;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.XStreamException;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import com.thoughtworks.xstream.security.PrimitiveTypePermission;
 
 public class Example implements Serializable, CommaSeparable {
 
@@ -69,5 +79,49 @@ public class Example implements Serializable, CommaSeparable {
         this.id = Integer.parseInt(fields[0]);
         this.name = fields[1];
         this.data = Double.parseDouble(fields[2]);
+    }
+
+    /**
+     * <p>
+     *     Serializes the example to an XML file.<br/>
+     *     <a href="https://www.baeldung.com/xstream-serialize-object-to-xml">
+     *         Serializing an object to XML with XStream</a>
+     * </p>
+     * @param example The example object to serialize
+     * @param fileName The name of the file to serialize the object to
+     * @throws IOException If the event of a problem creating or writing the file
+     * @throws XStreamException If there is a problem with the xml encoding
+     */
+    public static void serializeToXML(Example example, String fileName)
+            throws IOException, XStreamException {
+        XStream xStream = new XStream();
+        xStream.processAnnotations(Example.class);
+        OutputStream stream =
+                Files.newOutputStream(Paths.get(fileName));
+        stream.write(xStream.toXML(example).getBytes());
+        stream.close();
+    }
+
+    /**
+     * <p>
+     *      Deserializes and returns an instance of <code>Example</code> from
+     *      the given file.<br/>
+     *      <a href="https://www.baeldung.com/xstream-deserialize-xml-to-object">
+     *          Deserializing an object from XML using XStream</a>
+     * </p>
+     * @param fileName The name of the file to deserialize from
+     * @return A deserialized instance of the <code>Example</code> class
+     * @throws IOException If the file doesn't exist or cannot be opened
+     * @throws XStreamException If there is a problem parsing the XML
+     */
+    public static Example deserializeFromXML(String fileName)
+            throws IOException, XStreamException {
+        InputStream stream =
+                Files.newInputStream(Paths.get(fileName));
+        XStream xStream = new XStream();
+        xStream.allowTypesByWildcard(new String[]{"edu.cofc.johnson.**"});
+        xStream.processAnnotations(Example.class);
+        Example example = (Example) xStream.fromXML(stream);
+        return example;
     }
 }
